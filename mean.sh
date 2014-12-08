b84284bfd1fba5ecfad3af9222ce285d13588a9b
brownman <brownman556@gmail.com>
2014-12-08 19:30:45 +0200

    Update mean.sh

--- mean.sh
+++ mean.sh
@@ -1,7 +1,10 @@
 export VER_MEAN_CLI='0.9.3'
-
+step1(){
 npm update -g npm
-npm install -g grunt-cli bower 
+npm install -g grunt-cli 
+}
+step2(){
+npm install -g bower 
 npm install -g mean-cli@${VER_MEAN_CLI}
 
 echo $HOME
@@ -11,3 +14,5 @@ cd myApp
 npm -g install
 npm link 
 ( grunt test ) || ( echo never mind the test - try again later! )
+}
+step1
