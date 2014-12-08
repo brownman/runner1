015cc43e93b244a5ef323ea267b705879d4ca58a
brownman <brownman556@gmail.com>
2014-12-08 20:39:27 +0200

    testing-final: pp

--- circle.sh
+++ circle.sh
@@ -1,10 +1,18 @@
 dir1=$CIRCLE_ARTIFACTS
-docker run brownman/runner1 env &> $dir1/env.txt
-docker run brownman/runner1 bash -c 'ls `npm root -g`' &> $dir1/npm_packages1.txt
-ls `npm root -g` > $dir1/npm_packages2.txt
-cp /root/.bashrc $dir1/bashrc_root.txt
-cp /docker/.bashrc $dir1/bashrc_docker.txt
-ls `which grunt` -l >$dir1/grunt.txt
+
+run1(){
+ exec 2>&1
+ 
+ local args=( $@ )
+ local cmd=${args[@]}
+ docker run brownman/runner1 "$cmd"
+}
+run1 env > $dir1/env.txt
+run1 'bash -c ls `npm root -g`' > $dir1/npm_packages1.txt
+run1 'ls `npm root -g`' > $dir1/npm_packages2.txt
+run1 'cat /root/.bashrc' > $dir1/bashrc_root.txt
+run1 'cat /docker/.bashrc' > $dir1/bashrc_docker.txt
+run1 'ls `which grunt` -l' > $dir1/grunt.txt
 
 
 #docker run brownman/runner1 'apt-get install locate;updatedb; locate grunt-cli' &> /tmp/grunt2.txt
