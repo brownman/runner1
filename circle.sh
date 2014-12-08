65ce2788a3ca67f41522771641471a0fb34ae83d
brownman <brownman556@gmail.com>
2014-12-08 20:29:51 +0200

    Update circle.sh

--- circle.sh
+++ circle.sh
@@ -1,11 +1,13 @@
 dir1=$CIRCLE_ARTIFACTS
-docker run brownman/runner1 env &> /tmp/env.txt
-docker run brownman/runner1 grunt-cli &> /tmp/grunt.txt
+docker run brownman/runner1 env &> $dir1/env.txt
+docker run brownman/runner1 grunt-cli &> $dir1/grunt1.txt
+docker run brownman/runner1 bash -c 'grunt-cli' &> $dir1/grunt_cli.txt
+docker run brownman/runner1 bash -c 'ls `npm root -g`' &> $dir1/npm_packages1.txt
+ls `npm root -g` > $dir1/npm_packages2.txt
+cp /root/.bashrc $dir1/bashrc.txt
+
+
 #docker run brownman/runner1 'apt-get install locate;updatedb; locate grunt-cli' &> /tmp/grunt2.txt
 
  #sudo -u gitlab_ci_runner -H 'env' > /tmp/env.txt
-cp /tmp/env.txt $dir1/env.txt
-cp /tmp/grunt.txt $dir1/grunt.txt
-#cp /tmp/grunt2.txt $dir1/grunt2.txt
 
-ls `npm root -g` > $dir1/npm_packages.txt
