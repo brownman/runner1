dfd6f2674bd2373fb4ac00c4e3efff3a1880cd8c
brownman <brownman556@gmail.com>
2014-12-08 19:26:55 +0200

    testing-final: locate grunt-cli

--- circle.sh
+++ circle.sh
@@ -1,2 +1,6 @@
-echo hello world
-docker run -t brownman/runner1 bash -c 'grunt-cli'
+ docker run brownman/install_config_test env > /tmp/env
+ docker run brownman/install_config_test grunt-cli > /tmp/grunt
+
+ #sudo -u gitlab_ci_runner -H 'env' > /tmp/env.txt
+cp /tmp/env.txt $CIRCLE_ARTIFACTS/env.txt
+cp /tmp/grunt.txt $CIRCLE_ARTIFACTS/grunt.txt
