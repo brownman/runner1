78d3a974b14adb8d86c2153ac10b5b952a0fb13e
brownman <brownman556@gmail.com>
2014-12-08 19:00:09 +0200

    fix: mean-cli version
    we want to use a non-interactive vversion

--- mean.sh
+++ mean.sh
@@ -1,9 +1,13 @@
+export VER_MEAN_CLI='0.9.3'
+
 npm update -g npm
 npm install -g grunt-cli bower 
-npm install -g mean-cli 
+npm install -g mean-cli@${VER_MEAN_CLI}
+
+echo $HOME
 cd $HOME 
-mean-cli init myApp 
+mean init myApp 
 cd myApp 
 npm -g install
 npm link 
-echo grunt test
+( grunt test ) || ( echo never mind the test - try again later! )
