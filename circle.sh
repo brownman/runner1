7defc302c249d5dc56948fe50ab25d821543270b
brownman <brownman556@gmail.com>
2014-12-08 18:46:41 +0200

    Update circle.sh

--- circle.sh
+++ circle.sh
@@ -1,10 +1,2 @@
 echo hello world
-docker run -t brownman/runner1 <<START
-npm install -g grunt-cli bower mean-cli
-cd $HOME
-mean-cli init myApp
-cd myApp
-npm -g install 
-npm link
-grunt test
-START
+docker run -t brownman/runner1 bash -c 'grunt-cli'
