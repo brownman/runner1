045420a6387c5fbe2d81dc13d42f4305529e7b69
brownman <brownman556@gmail.com>
2014-12-08 17:22:57 +0200

    updated test

--- circle.sh
+++ circle.sh
@@ -1,1 +1,10 @@
 echo hello world
+docker run -t brownman/runner1 <<START
+npm install -g grunt-cli bower mean-cli
+cd $HOME
+mean-cli init myApp
+cd myApp
+npm -g install 
+npm link
+grunt test
+START
