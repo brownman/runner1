382533a24743098585012c4c611082d9851f4670
brownman <brownman556@gmail.com>
2014-12-09 13:04:29 +0200

    test: netstat

--- circle.sh
+++ circle.sh
@@ -8,13 +8,20 @@ run1(){
  local cmd="${args[@]}"
  echo cmd: $cmd
  docker run brownman/runner1 bash -c "$cmd" 1>$dir1/${str}.out.txt  2>$dir1/${str}.err.txt
+ echo -e '----\n err:' >> $dir1/${str}.out.txt
+ cat $dir1/${str}.err.txt >> $dir1/${str}.out.txt
+ cp $dir1/${str}.out.txt $dir1/${str}.txt
+ rm $dir1/${str}.err.txt
+ rm  $dir1/${str}.out.txt
 }
 
 run1 env env > $dir1/env.txt
+run1 netstat 'netstat -ntlp'
 run1 npm_packages1 'ls `npm root -g`'
-run1 npm_packages2 'ls `npm root -g`' 
 run1 bashrc_root 'cat /root/.bashrc'
-run1 bashrc_docker 'cat /docker/.bashrc' 
+run1 ps 'ps aux'
+
+
 #run1 grunt 'ls $(which grunt) -l'
 
 
