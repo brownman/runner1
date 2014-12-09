10095d716ba4535845d72a5a59bdfde24dd12784
brownman <brownman556@gmail.com>
2014-12-09 12:01:50 +0200

    Update circle.sh

--- circle.sh
+++ circle.sh
@@ -7,7 +7,7 @@ run1(){
  local args=( $@ )
  local cmd="${args[@]}"
  echo cmd: $cmd
- docker run brownman/runner1 "bash -c \"$cmd\"" 1>$dir1/${str}.out  2>$dir1/${str}.err
+ docker run brownman/runner1 "bash -c \"$cmd\"" 1>$dir1/${str}.out.txt  2>$dir1/${str}.err.txt
 }
 
 run1 env env > $dir1/env.txt
@@ -15,7 +15,7 @@ run1 npm_packages1 'ls `npm root -g`'
 run1 npm_packages2 'ls `npm root -g`' 
 run1 bashrc_root 'cat /root/.bashrc'
 run1 bashrc_docker 'cat /docker/.bashrc' 
-run1 grunt 'ls `which grunt` -l'
+#run1 grunt 'ls $(which grunt) -l'
 
 
 #docker run brownman/runner1 'apt-get install locate;updatedb; locate grunt-cli' &> /tmp/grunt2.txt
