b59ba1bd3d38c585888d3edffae1726d63f95c45
brownman <brownman556@gmail.com>
2014-12-09 16:48:55 +0200

    mission: this file will be triggered by every package commit

---
+++ runner.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+
+
+  
+commander(){
+  local args=($@)
+  local cmd="${args[@]}"
+  print_color_n 32 "[CMD]"
+  echo "$cmd" | pv -qL 10
+  print_line
+  eval "$cmd"
+  print_line
+}
+
+if [ $# -gt 0 ];then
+  args0=($@)
+  cmd0="${args[@]}"
+  commander "$cmd0"
+fi
+
+commander netstat -ntpl
+commander ps aux
+commander mkdir -p /root/myApp/packages/contrib/my_package
+commander cp * -rf /root/myApp/packages/contrib/my_package
+commander tree /root/myApp/packages/contrib/my_package
+commander cd /root/myApp
+commander /usr/bin/grunt test
