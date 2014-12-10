c128797fc85f9f2389afc4fffebd669f86d1acbd
brownman <brownman556@gmail.com>
2014-12-10 15:34:02 +0200

    mute noise

--- mean.sh
+++ mean.sh
@@ -1,23 +1,39 @@
 #export VER_MEAN_CLI='0.9.3'
+set -u
+exec 1>/dev/null
+
+
+trace(){
+  echo 1>&2 "[TRACE] $@"
+}
+
+commander(){
+    local args=( $@ )
+    local cmd="${args[@]}"
+    trace "[CMD] $cmd" 
+    set +e
+    eval "$cmd" || { exit 1; }
+}
 
 step1(){
-echo skipping npm update -g npm
-npm install -g grunt-cli 
+  trace skipping npm update -g npm
+  commander npm install -g grunt-cli 
 }
-step2(){
-npm install -g bower 
-npm install -g mean-cli
-#@${VER_MEAN_CLI}
 
+step2(){
+ #  cat /tmp/2.sh  | grep -i err | wc -l
+commander npm install -g bower 
+commander npm install -g mean-cli
+#@${VER_MEAN_CLI}
 echo $HOME
 cd $HOME 
-/usr/bin/yes | mean init myApp 
-mv y myApp
+echo -e '\n' | mean init myApp 
 cd myApp 
-npm -g install
-npm link 
+commander npm -g install
+commander npm link 
 #( grunt test ) || ( echo never mind the test - try again later! )
 }
+
 step3(){
   apt-get update -y 
   apt-get upgrade -y
@@ -26,5 +42,7 @@ step3(){
   updatedb
   locate grunt-cli
 }
+
+
 step1
 step2
